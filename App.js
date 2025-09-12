dcd27c4e8cf90dbeb54e020b5bf04422176b1b19
Neelam Sushanth <neelamsushanth2004@gmail.com>
2025-09-12 21:36:14 +0530

    lecture-4

--- App.js
+++ App.js
@@ -1,27 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const logo = (
-  <img src="https://th.bing.com/th/id/R.5eb1959636a370b661bc91940fe49cee?rik=DiotHJlkKQR6dg&riu=http%3a%2f%2fwww.datwebdigital.com%2fDWD%2fwp-content%2fuploads%2f2012%2f06%2flogo-design.jpg&ehk=fa8lsC0cm1nXH1dOqP%2f9dC1ohF3%2bcobEoqkMOaxrV2I%3d&risl=&pid=ImgRaw&r=0" />
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="/Logo.png" alt="Logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const searchBar = <input type="search" />;
-
-const userIcon = (
-  <img
-    src="https://th.bing.com/th/id/OIP.Ln_qrnMeEWlR-sIzaHn2fAHaHa?w=179&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3"
-    alt="user-icon"
-  />
-);
-
-const Header = () => (
-  <div className="header">
-    {logo}
-    {searchBar}
-    {userIcon}
-  </div>
-);
+const AppLayout = () => {
+  return (
+    <div className="appLayout">
+      <Header />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Header />);
+root.render(<AppLayout />);
---
+++ README.md
@@ -0,0 +1,15 @@
+Food Delivery App - QuickServe
+
+1. Planning 
+
+components of App (For Reference)
+Header
+ - logo
+ - nav items
+Body
+ - search
+ - Restaurant container
+ - Restaurant card
+Footer
+ - copyright
+ - links 
